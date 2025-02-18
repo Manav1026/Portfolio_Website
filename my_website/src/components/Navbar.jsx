557c3410b5b9d49fdf1ebe3662a753e5a2a500d1
Manav1026 <manav.dhami10@gmail.com>
2025-02-17 23:19:41 -0500

    Updated Navbar.jsx

--- my_website/src/components/Navbar.jsx
+++ my_website/src/components/Navbar.jsx
@@ -1,22 +1,15 @@
 export const Navbar = () => {
   return (
     <>
-      <nav className="bg-white shadow-md sticky top-0 z-50">
-        <div className="container mx-auto px-4 py-4 flex justify-between items-center">
-          <div className="text-gray-800 text-xl font-semibold">Manav Dhami</div>
-          <div className="space-x-6 text-gray-600 font-medium">
-            <a href="#home" className="hover:text-gray-900">
-              Home
-            </a>
-            <a href="#about" className="hover:text-gray-900">
-              About
-            </a>
-            <a href="#projects" className="hover:text-gray-900">
-              Projects
-            </a>
-            <a href="#contact" className="hover:text-gray-900">
-              Contact
-            </a>
+      <nav className="navbar">
+        <div className="container">
+          <div className="brand">Manav Dhami</div>
+          <div className="links">
+            <a href="home">Home</a>
+            <a href="skills">Skills</a>
+            <a href="projects">Projects</a>
+            <a href="qualification">Qualification</a>
+            <a href="contact">Contact</a>
           </div>
         </div>
       </nav>
